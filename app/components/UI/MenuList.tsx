f2923526481fe07e520b24ebded69ac4a9eeaa7b
caturhidayat <caturhidayat00@gmail.com>
2024-11-26 22:48:19 +0700

    style(app): MenuList change bg and color

--- app/components/UI/MenuList.tsx
+++ app/components/UI/MenuList.tsx
@@ -6,18 +6,18 @@ import {
 
 export default function MenuList() {
     return (
-        <div className="grid grid-cols-3 gap-4 lg:grid-cols-3 lg:gap-8 p-4">
-            <div className="h-24 rounded-lg bg-accent flex items-center justify-center">
-                <ClockArrowUp size={39} className="text-secondary-content" />
+        <div className="grid grid-cols-3 gap-4 lg:grid-cols-3 lg:gap-8 p-4 bg-base-200 px-2">
+            <div className="rounded-lg  flex items-center justify-center">
+                <ClockArrowUp size={39} className="text-primary" />
             </div>
-            <div className="h-24 rounded-lg bg-accent flex items-center justify-center">
+            <div className="rounded-lg  flex items-center justify-center">
                 <CircleArrowOutDownRight
                     size={39}
-                    className="text-secondary-content"
+                    className="text-primary"
                 />
             </div>
-            <div className="h-24 rounded-lg bg-accent flex items-center justify-center">
-                <CalendarCheck size={39} className="text-secondary-content" />
+            <div className="rounded-lg  flex items-center justify-center">
+                <CalendarCheck size={39} className="text-primary" />
             </div>
         </div>
     );
