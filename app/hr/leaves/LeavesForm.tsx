298fdd8fc1aca1911371f1767451993335a2bc79
caturhidayat <caturhidayat00@gmail.com>
2024-12-01 13:13:17 +0700

    feat(app): leave form request page design form input for leave request
    WIP

--- app/hr/leaves/LeavesForm.tsx
+++ app/hr/leaves/LeavesForm.tsx
@@ -1,54 +1,257 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Calendar } from "@/components/ui/calendar";
 
 const FormSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
+  // userId: z.string(),
+  startDate: z.date(),
+  endDate: z.date(),
+  reason: z.string(),
+  leaveType: z.string(),
+  status: z.string(),
+  approvedById: z.string(),
+  dayUsed: z.number(),
+  imageUrl: z.string(),
+});
+// userId       String
+//   user         User          @relation(fields: [userId], references: [id])
+//   startDate    BigInt?
+//   endDate      BigInt?
+//   reason       String?
+//   leaveType    LeaveType
+//   status       RequestStatus @default(PENDING)
+//   approvedById String?
+//   dayUsed      Float
+//   imageUrl     String?
 
+const LeaveType = [
+  "SICK",
+  "ANNUAL",
+  "UNPAID",
+  "MATERNITY",
+  "PATERNITY",
+  "BEREAVEMENT",
+  "STUDY",
+  "OTHER",
+];
+
+// SICK,
+//   ANNUAL,
+//   UNPAID
+//   MATERNITY
+//   PATERNITY
+//   BEREAVEMENT
+//   STUDY
+//   OTHER
 export function InputForm() {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      username: "",
+      // userId: "",
+      // startDate: new Date(),
+      // endDate: new Date(),
+      reason: "",
+      leaveType: "",
+      status: "",
+      approvedById: "",
+      dayUsed: 0,
+      imageUrl: "",
     },
-  })
+  });
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
-    console.log(data)
+    console.log(data);
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-        <FormField
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-5/6 space-y-2 m-auto"
+      >
+        {/* <FormField
           control={form.control}
-          name="username"
+          name="userId"
           render={({ field }) => (
             <FormItem>
               <FormLabel>Username</FormLabel>
               <FormControl>
                 <Input placeholder="shadcn" {...field} />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        /> */}
+
+        <FormField
+          control={form.control}
+          name="leaveType"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Leave Type</FormLabel>
+              <FormControl>
+                <Select>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Leave Type" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {LeaveType.map((leaveType) => (
+                      <SelectItem key={leaveType} value={leaveType}>
+                        {leaveType}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="startDate"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="grid">Start Date</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-full pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="p-0">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="endDate"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="grid">End Date</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-full pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="reason"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Reason</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="dayUsed"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Day Used</FormLabel>
+              <FormControl>
+                <Input type="number" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="imageUrl"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Image URL</FormLabel>
+              <FormControl>
+                <Input type="file" {...field} />
+              </FormControl>
               <FormMessage />
             </FormItem>
           )}
@@ -56,5 +259,5 @@ export function InputForm() {
         <Button type="submit">Submit</Button>
       </form>
     </Form>
-  )
+  );
 }
--- app/hr/leaves/page.tsx
+++ app/hr/leaves/page.tsx
@@ -1,11 +1,12 @@
 import { InputForm } from "./LeavesForm";
 
-
 export default function Leaves() {
-    return (
-        <div className="grid gap-4 m-auto p-4">
-            <div>Leaves</div>
-            <InputForm />
-        </div>
-    )
+  return (
+    <div className="grid gap-4 m-auto p-4">
+      <div className="text-2xl font-bold">Leave Request Form</div>
+      {/* <div className="grid"> */}
+        <InputForm />
+      {/* </div> */}
+    </div>
+  );
 }
