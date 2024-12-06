4e0b6639fcda792550b5e1e45e0d82c5b267e62f
caturhidayat <caturhidayat00@gmail.com>
2024-12-06 22:39:22 +0700

    refactor(app): navbar change text
    WIP

--- app/components/UI/Navbar.tsx
+++ app/components/UI/Navbar.tsx
@@ -1,21 +1,24 @@
 "use client";
 
 import { AuthContext } from "@/app/auth/auth-context";
+import { Squirrel } from "lucide-react";
 import { useContext } from "react";
 
 export default function Navbar() {
-    const isAuthenticated = useContext(AuthContext);
+  const isAuthenticated = useContext(AuthContext);
 
-    return (
-        <div>
-            {isAuthenticated ? (
-                <div className="navbar bg-base-100 fixed ">
-                    <div className="flex-1">
-                        <a className="btn btn-ghost text-2xl bg-gradient-to-r from-violet-600 via-teal-600 to-purple-600 bg-clip-text text-transparent">App Name</a>
-                    </div>
-                    <div className="flex-none"></div>
-                </div>
-            ) : null}
+  return (
+    <div>
+      {isAuthenticated ? (
+        <div className="navbar bg-base-100 fixed ">
+          <div className="flex-1">
+            <a className="btn btn-ghost text-2xl bg-gradient-to-r from-violet-600 via-teal-600 to-purple-600 bg-clip-text text-transparent">
+              [app name]
+            </a>
+          </div>
+          <div className="flex-none"></div>
         </div>
-    );
+      ) : null}
+    </div>
+  );
 }
