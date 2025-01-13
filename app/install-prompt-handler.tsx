02ba2a43ce612b7a4bdb9250dbedfd40822bdd37
caturhidayat <caturhidayat00@gmail.com>
2025-01-13 22:43:31 +0700

    style(app): toast install prompt redesign toast
    WIP

--- app/install-prompt-handler.tsx
+++ app/install-prompt-handler.tsx
@@ -48,21 +48,58 @@ export default function InstallPromptHandler() {
   return (
     <>
       {showInstallButton &&
+        // toast.custom((t) => (
+        //   <div
+        //     className={`${
+        //       t.visible ? "animate-enter" : "animate-leave"
+        //     } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+        //   >
+        //     <button
+        //       className="btn btn-primary btn-sm"
+        //       onClick={handleInstallClick}
+        //       // style={{ position: "fixed", bottom: 20, right: 20 }}
+        //     >
+        //       Install App
+        //     </button>
+        //   </div>
+        // ))
         toast.custom((t) => (
           <div
             className={`${
               t.visible ? "animate-enter" : "animate-leave"
             } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
           >
-            <button
-              className="btn btn-primary btn-sm"
-              onClick={handleInstallClick}
-              // style={{ position: "fixed", bottom: 20, right: 20 }}
-            >
-              Install App
-            </button>
+            <div className="flex-1 w-0 p-4">
+              <div className="flex items-start">
+                <div className="flex-shrink-0 pt-0.5">
+                  <button
+                    onClick={handleInstallClick}
+                    className="btn btn-primary btn-sm"
+                  >
+                    Install App
+                  </button>
+                </div>
+                <div className="ml-3 flex-1">
+                  <p className="text-sm font-medium text-gray-900">
+                    Install App
+                  </p>
+                  <p className="mt-1 text-sm text-gray-500">
+                    Install this app on your device
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="flex border-l border-gray-200">
+              <button
+                onClick={() => toast.dismiss(t.id)}
+                className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              >
+                Dismiss
+              </button>
+            </div>
           </div>
-        ))}
+        ))
+        }
     </>
   );
 }
