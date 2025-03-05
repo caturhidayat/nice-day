831d80560e00de666cc7ae7ac76f184412da96c3
caturhidayat <caturhidayat00@gmail.com>
2025-03-05 23:11:00 +0700

    refactor(app): temp remove sw and refine attendance preview WIP

--- next.config.js
+++ next.config.js
@@ -42,27 +42,17 @@ module.exports = async (phase) => {
   /** @type {import("next").NextConfig} */
   const nextConfig = {};
 
-  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
-    const withSerwist = (await import("@serwist/next")).default({
-      // Note: This is only an example. If you use Pages Router,
-      // use something else that works, such as "service-worker/index.ts".
-      swSrc: "app/sw.ts",
-      swDest: "public/sw.js",
-    });
-    return withSerwist(nextConfig);
-  }
+  // if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+  //   const withSerwist = (await import("@serwist/next")).default({
+  //     // Note: This is only an example. If you use Pages Router,
+  //     // use something else that works, such as "service-worker/index.ts".
+  //     swSrc: "app/sw.ts",
+  //     swDest: "public/sw.js",
+  //   });
+  //   return withSerwist(nextConfig);
+  // }
 
   return nextConfig;
 };
-
-// withSerwistInit({
-//   swSrc: "app/sw.ts",
-//   swDest: "public/sw.js",
-//   cacheOnNavigation: true,
-//   globDirectory: "dist/static",
-//   // Bomb has been planted
-//   maximumFileSizeToCacheInBytes: 7355608,
-//   reloadOnOnline: true,
-// });
 
 module.exports = nextConfig;
